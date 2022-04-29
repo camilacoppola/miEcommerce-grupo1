c94833f3ebb753e03fe4801a0316cea2a42464b6
camilacoppola <ccoppola@santandertecnologia.com.ar>
2022-04-29 10:41:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+
+app.set('views', './views/pages');
+app.set('view engine', 'ejs');
+app.use(express.static('assets'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+/* Ruta al index */
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+/* Ruta al login */
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+/* Ruta al product */
+app.get('/products', (req, res) => {
+    res.render('product');
+});
+
+/* Ruta al cart */
+app.get('/cart', (req, res) => {
+    res.render('cart');
+});
+
+/* Ruta al checkout */
+app.get('/checkout', (req, res) => {
+    res.render('checkout');
+});
+
+/* Ruta al register */
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+
+app.listen(PORT, () =>{
+    console.log('Servidor Levantado');
+})
+
