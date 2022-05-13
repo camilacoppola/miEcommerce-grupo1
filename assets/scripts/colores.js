dbe94a6a0d6f8ced88814e1a3002f269d027cdd0
ignachioz03 <icampot@santandertecnologia.com.ar>
2022-05-13 09:41:55 -0300

    subo editar perfil

---
+++ assets/scripts/colores.js
@@ -0,0 +1,18 @@
+//colores modo dark
+let mdColorNegroClaro = '#222222'    //Fondo
+let mdColorNegroOscuro = '#000000'   //Head y foot
+let mdColorGrisClaro = '#494F51'     //Cards y botones
+let mdColorGrisOscuro= '333333'      //Categoría barra y colores carrito
+let mdColorGris = '#999999'          //Textos e íconos
+
+//colores modo light
+let mlColorGris = '#EFEFEF'
+let mlColorGrisOscuro = '#494f51'
+let mlColorVerde = '#0cb093'
+let mlColorBlanco = '#ffffff'
+let mlColorNegro = '#000000'
+let mlColorRojo  = '#ff0000'
+
+window.addEventListener('load', () => {
+
+})
